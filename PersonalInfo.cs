c74bc2e55342e404e965bd9acc22b8672cafa3ef
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2023-11-18 22:28:59 -0500

    Changes committed

--- PersonalInfo.cs
+++ PersonalInfo.cs
@@ -6,13 +6,13 @@ namespace CSharp
     {
         static void Main()
         {
-            Console.WriteLine("Please enter your name");
-            string UserName = Console.ReadLine();
+            Console.WriteLine("Please enter your full name");
+            string fullName = Console.ReadLine();
 
             Console.WriteLine("Please enter your age");
             int YourAge = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("\nHello " + UserName + "\nMy age is " + YourAge);
+            Console.WriteLine("\nHello " + fullName + "\nMy age is " + YourAge);
             Console.ReadLine();
         }
     }
--- Practise.cs
+++ Practise.cs
@@ -1,37 +1,98 @@
 ﻿using System;
 
-namespace CSharp
+class Calculator
 {
-    class Practise
+    static void Main()
     {
-
-        public string Details(string name, int age, string country)
+        while (true)
         {
-            return "\nMy name is " + name + "\nMy age is " + age + "\nI am a resident of " + country;
+            Console.WriteLine("Simple Calculator");
+            Console.WriteLine("-----------------");
+            Console.WriteLine("1. Addition");
+            Console.WriteLine("2. Subtraction");
+            Console.WriteLine("3. Multiplication");
+            Console.WriteLine("4. Division");
+            Console.WriteLine("5. Exit");
+
+            Console.Write("Enter your choice (1-5): ");
+            string choice = Console.ReadLine();
+
+            if (choice == "5")
+            {
+                Console.WriteLine("Exiting the calculator. Goodbye!");
+                break;
+            }
+
+            double num1, num2, result;
+
+            Console.Write("Enter the first number: ");
+            if (!double.TryParse(Console.ReadLine(), out num1))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
+
+            Console.Write("Enter the second number: ");
+            if (!double.TryParse(Console.ReadLine(), out num2))
+            {
+                Console.WriteLine("Invalid input. Please enter a valid number.");
+                continue;
+            }
+
+            switch (choice)
+            {
+                case "1":
+                    result = num1 + num2;
+                    Console.WriteLine($"Result: {num1} + {num2} = {result}");
+                    break;
+                case "2":
+                    result = num1 - num2;
+                    Console.WriteLine($"Result: {num1} - {num2} = {result}");
+                    break;
+                case "3":
+                    result = num1 * num2;
+                    Console.WriteLine($"Result: {num1} * {num2} = {result}");
+                    break;
+                case "4":
+                    if (num2 != 0)
+                    {
+                        result = num1 / num2;
+                        Console.WriteLine($"Result: {num1} / {num2} = {result}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Error: Division by zero is undefined.");
+                    }
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
+                    break;
+            }
+
+            Console.WriteLine();
         }
-
-
-
-        static void Main()
-        {
-            Console.WriteLine("Please enter your name");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Please enter your age");
-            int age = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please enter your name of resident country");
-            string country = Console.ReadLine();
-
-            Practise myObject = new Practise();
-            
-            string result = myObject.Details(name, age, country);
-
-            Console.WriteLine(result);
-
-            Console.ReadKey();
-        }
-
-
     }
 }
+
+
+
+//User Define:-
+
+//All Variables Name:-  choice, num1 , num2, result
+//All Methods Name:-  Main method
+//All Classes Name:-  calculatorwitch
+//All Namespaces Name:- 
+//All Datatypes Name:- string, double
+//Jumping Statement:- break , continue
+//Conditional Statement:- if, else, switch, case
+//Loop:- while
+//Struct name:-
+//Enum Name:-
+//All keywords name:- class, static, void, while, string, double, break, continue, if, else, switch, case
+//Operators Name:- == , != , + , - , * , /
+
+
+
+
+
+
