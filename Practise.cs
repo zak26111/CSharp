f7045b2f8e3a653ba4e140cd17f9b404f0aa9bd0
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2023-11-18 23:36:18 -0500

    Changes committed - 2336

--- Practise.cs
+++ Practise.cs
@@ -1,98 +1,31 @@
 ﻿using System;
 
-class Calculator
+namespace CSharp
 {
-    static void Main()
+    public class PractiseClass
     {
-        while (true)
+        public static void StaticMethod()
         {
-            Console.WriteLine("Simple Calculator");
-            Console.WriteLine("-----------------");
-            Console.WriteLine("1. Addition");
-            Console.WriteLine("2. Subtraction");
-            Console.WriteLine("3. Multiplication");
-            Console.WriteLine("4. Division");
-            Console.WriteLine("5. Exit");
+            Console.WriteLine("This method is static");
+        }
 
-            Console.Write("Enter your choice (1-5): ");
-            string choice = Console.ReadLine();
+        public void NonStaticMethod()
+        {
+            Console.WriteLine("This method is non-static");
+        }
+    }
 
-            if (choice == "5")
-            {
-                Console.WriteLine("Exiting the calculator. Goodbye!");
-                break;
-            }
+    class Practise
+    {
+        static void Main()
+        {
+            PractiseClass.StaticMethod();
 
-            double num1, num2, result;
+            PractiseClass instance = new PractiseClass();
+            instance.NonStaticMethod();
 
-            Console.Write("Enter the first number: ");
-            if (!double.TryParse(Console.ReadLine(), out num1))
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
-                continue;
-            }
+            Console.ReadKey();
 
-            Console.Write("Enter the second number: ");
-            if (!double.TryParse(Console.ReadLine(), out num2))
-            {
-                Console.WriteLine("Invalid input. Please enter a valid number.");
-                continue;
-            }
-
-            switch (choice)
-            {
-                case "1":
-                    result = num1 + num2;
-                    Console.WriteLine($"Result: {num1} + {num2} = {result}");
-                    break;
-                case "2":
-                    result = num1 - num2;
-                    Console.WriteLine($"Result: {num1} - {num2} = {result}");
-                    break;
-                case "3":
-                    result = num1 * num2;
-                    Console.WriteLine($"Result: {num1} * {num2} = {result}");
-                    break;
-                case "4":
-                    if (num2 != 0)
-                    {
-                        result = num1 / num2;
-                        Console.WriteLine($"Result: {num1} / {num2} = {result}");
-                    }
-                    else
-                    {
-                        Console.WriteLine("Error: Division by zero is undefined.");
-                    }
-                    break;
-                default:
-                    Console.WriteLine("Invalid choice. Please enter a number between 1 and 5.");
-                    break;
-            }
-
-            Console.WriteLine();
         }
     }
 }
-
-
-
-//User Define:-
-
-//All Variables Name:-  choice, num1 , num2, result
-//All Methods Name:-  Main method
-//All Classes Name:-  calculatorwitch
-//All Namespaces Name:- 
-//All Datatypes Name:- string, double
-//Jumping Statement:- break , continue
-//Conditional Statement:- if, else, switch, case
-//Loop:- while
-//Struct name:-
-//Enum Name:-
-//All keywords name:- class, static, void, while, string, double, break, continue, if, else, switch, case
-//Operators Name:- == , != , + , - , * , /
-
-
-
-
-
-
