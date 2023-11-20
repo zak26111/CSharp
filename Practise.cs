264cf20d3a48dfa47f617579e05ebbd72012e6b7
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2023-11-19 23:27:37 -0500

    Committed Chnages (2327)

--- Practise.cs
+++ Practise.cs
@@ -1,31 +1,48 @@
-﻿using System;
+﻿//using System;
 
-namespace CSharp
+//namespace CSharp
+//{
+//    public class PractiseClass
+//    {
+//        public static void StaticMethod()
+//        {
+//            Console.WriteLine("This method is static");
+//        }
+
+//        public void NonStaticMethod()
+//        {
+//            Console.WriteLine("This method is non-static");
+//        }
+//    }
+
+//    class Practise
+//    {
+//        static void Main()
+//        {
+//            PractiseClass.StaticMethod();
+
+//            PractiseClass instance = new PractiseClass();
+//            instance.NonStaticMethod();
+
+//            Console.ReadKey();
+
+//        }
+//    }
+//}
+
+using System;
+
+class Practise
 {
-    public class PractiseClass
+    static void Main()
     {
-        public static void StaticMethod()
-        {
-            Console.WriteLine("This method is static");
-        }
+        int num = 100;
 
-        public void NonStaticMethod()
-        {
-            Console.WriteLine("This method is non-static");
-        }
-    }
+        Console.WriteLine(num);
+        num = 200;
+        num = 300;
 
-    class Practise
-    {
-        static void Main()
-        {
-            PractiseClass.StaticMethod();
-
-            PractiseClass instance = new PractiseClass();
-            instance.NonStaticMethod();
-
-            Console.ReadKey();
-
-        }
+        Console.WriteLine(num);
+        Console.ReadKey();
     }
 }
