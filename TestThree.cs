33f042c83e860c46c4c0de2a6fe2f110c749f76e
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2024-02-21 20:29:40 -0500

    Add

---
+++ TestThree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Net.NetworkInformation;
+
+namespace CSharp
+{
+    class TestThree
+    {
+        static void Main()
+        {
+            StartAgain:
+            Console.WriteLine("\nPlease enter your name");
+            string userName = Console.ReadLine();
+
+            if(!string.IsNullOrEmpty(userName) && userName.Length <= 10)
+            {
+                int lastNumber = userName.Length - 1;
+
+                string FormatName = char.ToUpper(userName[0]) + userName.Substring(1, lastNumber - 1).ToLower() + char.ToUpper(userName[lastNumber]);
+
+                Console.WriteLine("\nMy name is: " + FormatName);
+            }
+
+            else if(string.IsNullOrEmpty(userName))
+            {
+                Console.WriteLine("\n!You didn't enter anything in your name!");
+                goto StartAgain;
+            }
+
+            else
+            {
+                Console.WriteLine("\n!Please enter a valid name with the maximum of 10 letters!");
+                goto StartAgain;  
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
