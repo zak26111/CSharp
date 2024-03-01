992e0829687fdda7fa8c99c4fedd72baa7046197
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2024-02-29 20:00:40 -0500

    Add

---
+++ Test1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace CSharp
+{
+    class Test1
+    {
+        static void Main()
+        {
+            int[,] myArray = new int[3, 4];
+
+            for (int i = 0; i < 3; i++) 
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    myArray[i, j] = i * 4 + j + 1;
+                }
+            }
+
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    Console.WriteLine(myArray[i, j]);
+                }
+
+                Console.WriteLine();
+                Console.ReadKey();
+            }
+        }
+    }
+}
