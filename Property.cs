ee68abb26319bff0d443bd18f29634c1f9b0a0e2
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2024-03-01 22:22:55 -0500

    property

---
+++ Property.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CSharp
+{
+    class First
+    {
+        int magicNumber = 10;
+
+        public int MyProperty
+        {
+            get
+            {
+                return magicNumber;
+            }
+        }
+
+
+    }
+
+    class Second
+    {
+        static void Main()
+        {
+            First first = new First();
+            Console.WriteLine(first.MyProperty);
+
+            Console.ReadKey();
+        }
+    }
+}
