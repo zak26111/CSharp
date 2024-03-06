026f9ab9393a6b213f96f30bb82b38f4383e87f1
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2024-03-05 19:57:17 -0500

    add

--- Property.cs
+++ Property.cs
@@ -27,35 +27,50 @@
 //    }
 //}
 
+//using System;
+
+//namespace CSharp
+//{
+//    class PropertyOne
+//    {
+//        private string name = "Zain Ali";
+
+//        public string Name
+//        {
+//            get
+//            {
+//                return name;
+//            }
+
+//            //set
+//            //{ 
+//            //    name = value;
+//            //}
+//        }
+//    }
+
+//    class PropertyTwo
+//    {
+//        static void Main()
+//        {
+//            PropertyOne property = new PropertyOne();
+//            //property.Name = "Ali";
+//            Console.WriteLine(property.Name);
+//            Console.ReadKey();
+//        }
+//    }
+//}
+
 using System;
 
 namespace CSharp
 {
-    class PropertyOne
-    {
-        private string name = "Zain Ali";
-
-        public string Name
-        {
-            get
-            {
-                return name;
-            }
-
-            //set
-            //{ 
-            //    name = value;
-            //}
-        }
-    }
-
-    class PropertyTwo
+    class Property
     {
         static void Main()
         {
-            PropertyOne property = new PropertyOne();
-            //property.Name = "Ali";
-            Console.WriteLine(property.Name);
+            int i;
+            Console.WriteLine(i);
             Console.ReadKey();
         }
     }
