80a62c9db945369ab8867b9248396ebf8c5c66cc
Zain Ali Khan <zain_ali_khan_26111@yahoo.com>
2024-03-06 21:29:26 -0500

    indexers

--- Property.cs
+++ Property.cs
@@ -27,53 +27,63 @@
 //    }
 //}
 
-//using System;
-
-//namespace CSharp
-//{
-//    class PropertyOne
-//    {
-//        private string name = "Zain Ali";
-
-//        public string Name
-//        {
-//            get
-//            {
-//                return name;
-//            }
-
-//            //set
-//            //{ 
-//            //    name = value;
-//            //}
-//        }
-//    }
-
-//    class PropertyTwo
-//    {
-//        static void Main()
-//        {
-//            PropertyOne property = new PropertyOne();
-//            //property.Name = "Ali";
-//            Console.WriteLine(property.Name);
-//            Console.ReadKey();
-//        }
-//    }
-//}
-
 using System;
-using System.Collections.Generic;
+using System.Linq;
 
 namespace CSharp
 {
-    class Property
+    class PropertyOne
+    {
+        private string name = "";
+        private string message = "";
+
+        string[] names = { "Zain", "David", "Prashant", "Afroz", "Norika" };
+
+        public string Name
+        {
+            get
+            {
+                if (name.Length > 0)
+                    return name;
+                else
+                    return "No name value found";
+            }
+
+            set
+            {
+                name = value;
+            }
+        }
+
+        public string this[string name]
+        {
+            get 
+            {
+                if(names.Contains(name))
+                    message="Your name exist into our system.";
+                else
+                    message= "Your name does not exist into our system.";
+                return message; 
+            }
+            //set 
+            //{
+
+            //}
+
+        }
+    }
+
+    class PropertyTwo
     {
         static void Main()
         {
-            HashSet<int> hs = new HashSet<int>() { 1, 1, 2, 3, 3 };
-
-foreach (var val in hs)
-                Console.Write(val);
+            PropertyOne property = new PropertyOne();
+            //property.Name = "Ali";
+            //Console.WriteLine(property.Name);
+            string result = property["Zain123"];
+            Console.WriteLine(result); 
+            Console.ReadKey();
         }
     }
 }
+
